69ef353e245d4cddf2f2c8cae66ecad8400cb9e0
shiva <shivam212002@gmail.com>
2023-12-25 01:10:15 +0530

    add render url

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,44 @@
 import express from 'express';
-import bodyParser from 'body-parser'; 
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import connectDB from './db/connectDb.js';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
 import userPosts from './routes/userPosts.js';
-import {v2 as cloudinary} from 'cloudinary';
-
+import { v2 as cloudinary } from 'cloudinary';
+import cors from 'cors';
 
 dotenv.config();
 connectDB();
-const app=express();
-const PORT =process.env.PORT||10000;
+const app = express();
+const PORT = process.env.PORT || 10000;
+
 app.use((req, res, next) => {
   res.type('application/javascript');
   next();
 });
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET 
-  });
-  //middlewares
-app.use(express.json({limit:"50mb"})); //parse JSON data in req.body!!
-app.use(express.urlencoded({extended:true}));
 
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+// Middlewares
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//Routes
-app.use("/api/users",userRoutes);
-app.use("/api/posts",userPosts);
+// Enable CORS for a specific origin
+app.use(cors({
+  origin: 'https://threads-sigma-brown.vercel.app',
+  credentials: true, // enable set cookie
+}));
 
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/posts", userPosts);
 
-app.listen(PORT,()=>{
-    console.log(`Server started at  http://localhost:${PORT }`);
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
 });
